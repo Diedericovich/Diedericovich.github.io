f6048cea2eae11f744cfd427eb5c4366b98e7cc4
Diederik Van Gogh <doctor_music@hotmail.com>
2021-03-15 22:22:18 +0100

    Update 1.1

--- JS/Myweb.js
+++ JS/Myweb.js
@@ -1,5 +1,20 @@
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-// document.querySelectorAll('.ripple').forEach((cardRipple) => {
-//     new mdb.Ripple(cardRipple, { color: 'light' })
-//   });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
